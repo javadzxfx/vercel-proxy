220387ebd7e0eb4bb5ec7c3441cc1fbcd93e0098
javadzxfx <javadnyknamm@gmail.com>
2026-05-22 13:10:16 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const targetUrl = new URL(req.url, 'http://localhost');
-  targetUrl.hostname = '127.0.0.1';
-  targetUrl.port = '80';
+  targetUrl.hostname = '66.92.161.220';
+  targetUrl.port = '8080';
   const response = await fetch(targetUrl.toString(), {
     method: req.method,
     headers: req.headers,
